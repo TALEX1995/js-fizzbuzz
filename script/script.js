470cf6a7382a37d46c11521fcf8bcd2e20bdac3b
TALEX1995 <tanase.alex@libero.it>
2023-05-30 16:49:28 +0200

    Added Cicle and Conditions

--- script/script.js
+++ script/script.js
@@ -1,2 +1,24 @@
 console.log('JS OK');
 
+
+// Creo una variabile con scope globale che poi andro a stampare in console
+
+let numberFizzBuzz = ''
+
+
+// Preparo il ciclo
+for (let i = 1; i <= 100; i++) {
+
+    // Creo la condizione
+    if (!(i % 5) && !(i % 3)) {
+        numberFizzBuzz += ' FizzBuzz'
+    } else if (!(i % 5)) {
+        numberFizzBuzz += ' Buzz'
+    } else if (!(i % 3)) {
+        numberFizzBuzz += ' Fizz'
+    } else {
+        numberFizzBuzz += ' ' + i
+    }
+}
+
+console.log(numberFizzBuzz);
